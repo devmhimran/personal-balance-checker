aafa802d3a7b22f7f09403f3b23c5f9bccc393f1
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-02-17 22:55:53 +0600

    Added Comment In All Files

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,8 @@ function inputValue(inputId, invalidMessage){
     const input = document.getElementById(inputId);
     const inputValue = input.value;
     const inputValueParse = parseFloat(inputValue);
-    // return inputValueParse;  
 
+    // Input Field Validation
     if(inputValue == ''){
         document.getElementById(inputId+'-message').innerText = invalidMessage + ' is empty';
     }else if( inputValueParse <= 0 ){
@@ -19,74 +19,69 @@ function inputValue(inputId, invalidMessage){
     }  
 }
 
-
+// Show All Calculation Output
 function showCalculation(outputId, expense){
     const outputText = document.getElementById(outputId);
     outputText.innerText = expense;
     return outputText;
 }
 
-function expenseCalculation(food, rent, clothes){
-    const totalExpenseCalculation = food + rent + clothes;
-}
-
+// Expense Button Action
 document.getElementById('expense-button').addEventListener('click', function(){
     const income = inputValue('income-input', 'Income');
     const foodExpense = inputValue('food-input', 'Food expense');
     const rentExpense = inputValue('rent-input', 'Rent expense');
     const clothesExpense = inputValue('clothes-input', 'Clothes expense');
-    // const totalExpense = expenseCalculation(foodExpense, rentExpense, clothesExpense);
     const totalExpense = foodExpense + rentExpense + clothesExpense ;
     const balance = income - totalExpense;
+
+    // Check Nan Condition
     if( isNaN(foodExpense) || isNaN(rentExpense) || isNaN(clothesExpense) || isNaN(totalExpense)){
-        const expenseOutput = showCalculation('total-expense', '');
+        showCalculation('total-expense', '');
     }else{
-        const expenseOutput = showCalculation('total-expense','Total Expenses: '+totalExpense);
+        showCalculation('total-expense','Total Expenses: '+totalExpense);
     }
+    // Check Nan Condition
     if( isNaN(income) || isNaN(totalExpense)){
         showCalculation('total-balance', '');
     }else if( balance < 0 ){
         showCalculation('total-balance', '');
         showCalculation('balance-message','Insufficient Balance, Your expense is too much!!!');
-        console.log(balanceOutput);
     }else{
         showCalculation('total-balance','Balance: '+balance);
         showCalculation('balance-message','');
     }
-    console.log('Total Expense',totalExpense);
 });
 
+// Saving Button Action
 document.getElementById('saving-button').addEventListener('click', function(){
     const saving = inputValue('saving-input', 'Saving');
     const income = inputValue('income-input', 'Income');
     const foodExpense = inputValue('food-input', 'Food expense');
     const rentExpense = inputValue('rent-input', 'Rent expense');
     const clothesExpense = inputValue('clothes-input', 'Clothes expense');
+    // Total Expense Calculation
     const totalExpense = foodExpense + rentExpense + clothesExpense;
+    // Total Balance Expense
     const balance = income - totalExpense;
     const savingAmount = (income/100)*saving;
     const remainingBalance = balance - savingAmount;
 
+    // Check Nan Condition
     if( isNaN(savingAmount) ){
         showCalculation('saving-amount', '');
     }else{
         showCalculation('saving-amount', 'Saving Amount: ' + savingAmount);
     }
 
+    // Check Nan Condition
     if( isNaN(remainingBalance) ){
         showCalculation('remaining-amount', '');
     }else if( balance < savingAmount){
         showCalculation('remaining-amount', '');
         showCalculation('saving-invalid-message', 'You are not eligible for savings!!!');
-    }else{
-        
+    }else{    
         showCalculation('remaining-amount', 'Remaining Balance: ' + remainingBalance);
         showCalculation('saving-invalid-message', '');
     }
-
-    // else if( balance < remainingBalance){
-    //     showCalculation('saving-amount', '');
-    //     showCalculation('balance-message','You are not eligible for savings!!!');
-    // }
-    console.log( typeof savingCalculation);
 });
